a301c913ec25add78675069e7deff74fafe694a9
AmauriBlanco <blancoamauri@gmail.com>
2022-08-29 15:52:10 -0300

    semana 3 - aula 11

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { DiaristaModule } from './diarista/diarista.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    DiaristaModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'luhouse45',
+      database: 'curso_nest',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/diarista/diarista.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param, Render } from '@nestjs/common';
+
+@Controller('diarista')
+export class DiaristaController {
+  @Get(':id')
+  @Render('home')
+  root(@Param('id') id: number) {
+    const diaristas = [
+      {
+        nome: 'Paulo',
+      },
+      {
+        nome: 'Bruna',
+      },
+    ];
+    return { diarista: diaristas[id] };
+  }
+}
