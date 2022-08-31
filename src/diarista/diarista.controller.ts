d1ec9c9aacf72a7159994c676c98745f999d4d3a
AmauriBlanco <blancoamauri@gmail.com>
2022-08-31 16:29:00 -0300

    semana 3 - aula 17

--- src/diarista/diarista.controller.ts
+++ src/diarista/diarista.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Diarista } from './diarista.entity';
 
-@Controller('diarista')
+@Controller('diaristas')
 export class DiaristaController {
   constructor(
     @InjectRepository(Diarista)
@@ -12,6 +12,18 @@ export class DiaristaController {
   @Get()
   @Render('listar_diaristas')
   async listarDiaristas() {
-    return { diaristas: await this.diaristaRepository.find() };
+    return {
+      diaristas: await this.diaristaRepository.find(),
+      titulo: 'Lista de Diaristas',
+    };
+  }
+
+  @Get(':id')
+  @Render('detalhes')
+  async exibirDiarista(@Param('id') id: number) {
+    return {
+      diarista: await this.diaristaRepository.findOneBy({ id: id }),
+      titulo: 'Diarista',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as exphbs from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }));
+  await app.listen(3000);
 }
 bootstrap();
